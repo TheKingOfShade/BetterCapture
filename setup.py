0bfa68ab9285dbc832243e5468db01f6b730eee1
Cameron <CameronD.Barts@gmail.com>
2018-02-13 22:40:24 -0800

    Added Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BetterCapture',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='TheKingOfShade',
+    author_email='CameronD.Barts@gmail.com',
+    description='', install_requires=['httpagentparser', 'scapy==2.4.0rc3']
+)
